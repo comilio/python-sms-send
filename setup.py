ffa9d1447d72583288c15f14d7c48fa30eb8aee1
Franco Dalfovo <dalfovo.franco@gmail.com>
2018-08-27 13:09:55 +0200

    Updated long description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="Comilio",
     version="1.0.0",
     description="Client library to send SMS using Comilio SMS Gateway API (https://www.comilio.it)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Comilio",
     author_email="tech@comilio.it",
     license="MIT",
